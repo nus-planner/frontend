bd80191451be9ef12d3c21e3b59ecee90d65aef7
Ryan Peh <qwertz0808@gmail.com>
2022-10-03 14:45:16 +0800

    Add typescript

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        </h1>g
 
         <p className={styles.description}>
           Get started by editing{' '}
