9c3a83837decd95b3d9f079f755dbe0d7d212968
Ryan Peh <qwertz0808@gmail.com>
2022-10-04 17:06:47 +0800

    Add chakra ui

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,9 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>g
+        </h1>
+
+        
 
         <p className={styles.description}>
           Get started by editing{' '}
