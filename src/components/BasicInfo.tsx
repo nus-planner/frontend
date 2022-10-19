1f2d32a2f2ae0ec035a5af25509979f485a43654
yaoyuan <yaoyuan@u.nus.edu>
2022-10-19 23:22:05 +0800

    Refactor basic info out

--- src/components/BasicInfo.tsx
+++ src/components/BasicInfo.tsx
@@ -1,41 +1,41 @@
-import {
-  Select,
-  Input,
-  FormControl,
-  FormLabel,
-  FormErrorMessage,
-  FormHelperText,
-  HStack,
-} from "@chakra-ui/react";
+import { Select, FormControl, HStack, Heading } from "@chakra-ui/react";
 
 import { majors, specialisations } from "../constants/dummyModuleData";
+import { useState, SetStateAction } from "react";
 
-import React, { useState} from "react";
+const BasicInfo = () => {
+  // Basic info of the user
+  const years: number[] = [];
+  const currYear = new Date().getFullYear();
+  // Assume a student stays in NUS for at most 5 years
+  for (let i = 0; i < 5; i++) {
+    years.push(currYear - i);
+  }
 
-const years = [1,2,3,4];
-const currYear = new Date().getFullYear();
-
-const BasicInfo = (currYear: number) => {
   const [year, setYear] = useState("");
   const [major, setMajor] = useState("");
   const [specialisation, setSpecialisation] = useState("");
-
-  const handleYearChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+  const handleYearChange = (event: {
+    target: { value: SetStateAction<string> };
+  }) => {
     setYear(event.target.value);
   };
-
-  const handleMajorChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+  const handleMajorChange = (event: {
+    target: { value: SetStateAction<string> };
+  }) => {
     setMajor(event.target.value);
   };
 
-  const handleSpecialisationChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
-    setSpecialisation(event.target.value);
-  };
-
   return (
-    <HStack>
+    <HStack spacing={"1rem"}>
+      <Heading fontSize={"2xl"} fontWeight={"bold"} fontFamily={"body"}>
+        NUS Planner
+      </Heading>
       <FormControl w="-moz-fit-content">
-        <Select placeholder="Choose your enrollment year" onChange={handleYearChange}>
+        <Select
+          placeholder="Choose your enrollment year"
+          onChange={handleYearChange}
+        >
           {years.map((year) => (
             <option key={year}>
               AY{year}/{year + 1}
@@ -49,19 +49,6 @@ const BasicInfo = (currYear: number) => {
           <Select placeholder="Choose your major" onChange={handleMajorChange}>
             {majors.map((major) => (
               <option key={major}>{major}</option>
-            ))}
-          </Select>
-        </FormControl>
-      )}
-
-      {year && major && (
-        <FormControl w="-moz-fit-content">
-          <Select
-            placeholder="Choose your focus area"
-            onChange={handleSpecialisationChange}
-          >
-            {specialisations.map((specialisation) => (
-              <option key={specialisation}>{specialisation}</option>
             ))}
           </Select>
         </FormControl>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,52 +1,16 @@
 import {
-  Heading,
-  Select,
-  HStack,
   Stack,
   Divider,
-  FormControl,
 } from "@chakra-ui/react";
-import { useState, useCallback, useEffect, SetStateAction } from "react";
-import {
-  majors,
-  specialisations,
-} from "../constants/dummyModuleData";
+import { useState, useCallback, useEffect } from "react";
 import PlannerComponent from "../components/Planner";
 import * as models from "../models";
-
+import BasicInfo from "../components/BasicInfo";
 
 const Home = () => {
   // Helper function to help refresh since react-beautiful-dnd can't detect some changes
   const [, updateState] = useState<{}>();
   const forceUpdate = useCallback(() => updateState({}), []);
-
-
-  // Basic info of the user
-  const years = [];
-  const currYear = new Date().getFullYear();
-  // Assume a student stays in NUS for at most 5 years
-  for (let i = 0; i < 5; i++) {
-    years.push(currYear - i);
-  }
-
-  const [year, setYear] = useState("");
-  const [major, setMajor] = useState("");
-  const [specialisation, setSpecialisation] = useState("");
-  const handleYearChange = (event: {
-    target: { value: SetStateAction<string> };
-  }) => {
-    setYear(event.target.value);
-  };
-  const handleMajorChange = (event: {
-    target: { value: SetStateAction<string> };
-  }) => {
-    setMajor(event.target.value);
-  };
-  const handleSpecialisationChange = (event: {
-    target: { value: SetStateAction<string> };
-  }) => {
-    setSpecialisation(event.target.value);
-  };
 
   // TODO: Set up the following to change when corresponding plan is selected
   const [mainViewModel, setMainViewModel] = useState(
@@ -64,50 +28,7 @@ const Home = () => {
 
   return (
     <Stack padding="1rem">
-      <HStack spacing={"3rem"}>
-        <Heading fontSize={"2xl"} fontWeight={"bold"} fontFamily={"body"}>
-          NUS Planner
-        </Heading>
-        <FormControl w="-moz-fit-content">
-          <Select
-            placeholder="Choose your enrollment year"
-            onChange={handleYearChange}
-          >
-            {years.map((year) => (
-              <option key={year}>
-                AY{year}/{year + 1}
-              </option>
-            ))}
-          </Select>
-        </FormControl>
-
-        {year && (
-          <FormControl w="-moz-fit-content">
-            <Select
-              placeholder="Choose your major"
-              onChange={handleMajorChange}
-            >
-              {majors.map((major) => (
-                <option key={major}>{major}</option>
-              ))}
-            </Select>
-          </FormControl>
-        )}
-
-        {year && major && (
-          <FormControl w="-moz-fit-content">
-            <Select
-              placeholder="Choose your focus area"
-              onChange={handleSpecialisationChange}
-            >
-              {specialisations.map((specialisation) => (
-                <option key={specialisation}>{specialisation}</option>
-              ))}
-            </Select>
-          </FormControl>
-        )}
-      </HStack>
-
+      <BasicInfo/>
       <div />
       <Divider />
       <PlannerComponent
