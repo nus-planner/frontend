775007994c377ae855b4899f7ea2d5900ac2904c
sunjc826 <sunjc826@gmail.com>
2022-10-26 16:09:56 +0800

    Drill title down into stateful basket

--- src/models/input.ts
+++ src/models/input.ts
@@ -212,10 +212,12 @@ export class ValidatorState implements Hydratable {
       if (!this.states.has(basketOption.state)) {
         this.states.set(basketOption.state, new baskets.BasketState());
       }
-      basket = new baskets.StatefulBasket(
+      const statefulBasket = new baskets.StatefulBasket(
         basket,
         this.states.get(basketOption.state),
       );
+      statefulBasket.basket.title = basketOption.title || "";
+      basket = statefulBasket;
     }
 
     if (basketOption.description) {
@@ -238,6 +240,9 @@ export class ValidatorState implements Hydratable {
     // In other words, an explicitly specified title will be able to override the label
     if (basket.title === "") {
       basket.title = label;
+      if (basket instanceof baskets.StatefulBasket) {
+        basket.basket.title = label;
+      }
     }
     return basket;
   }
