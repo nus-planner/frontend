0f36aedd17e29a575f1e1242867d875927c0754b
yaoyuan <yaoyuan@u.nus.edu>
2022-10-28 23:22:41 +0800

    Add virtualized list for dropdown

--- src/components/ModuleDropdown.tsx
+++ src/components/ModuleDropdown.tsx
@@ -1,12 +1,11 @@
 import Select, { ActionMeta, SingleValue } from "react-select";
+import { WindowedMenuList } from "react-windowed-select";
 import { FormControl, Text, useDisclosure } from "@chakra-ui/react";
 import { fetchBasicModuleInfo } from "../api/moduleAPI";
 import { Module } from "../interfaces/planner";
 import * as models from "../models";
 import { useAppContext } from "./AppContext";
 import { DEFAULT_MODULE_COLOR, moduleColor } from "../constants/moduleColor";
-import { labelModules } from "../utils/plannerUtils";
-import { Context } from "@dnd-kit/sortable/dist/components";
 import React, { useEffect } from "react";
 
 interface ModuleDropdownProps {
@@ -174,11 +173,7 @@ const ModuleDropdown = ({
       <FormControl>
         <Select
           options={[{ options: options, label: module.code.slice(1, 4) }]}
-          placeholder={
-            module.name === "Unrestricted Electives"
-              ? "Key in a module"
-              : "Select a module"
-          }
+          placeholder={"Select a module"}
           value={
             !!underlyingModule
               ? {
@@ -195,6 +190,7 @@ const ModuleDropdown = ({
           menuIsOpen={isOpen}
           onMenuOpen={onOpen}
           onMenuClose={onClose}
+          components={{ MenuList: WindowedMenuList }}
         />
       </FormControl>
       {selectedModuleNameDisplay}
