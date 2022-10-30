e4ad907e7e92411fd77fbf20079de057864274ac
Ryan Peh <qwertz0808@gmail.com>
2022-10-30 12:28:29 +0800

    Change default theme to light mode

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,30 +1,43 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document'
-import React from 'react'
-import { resetServerContext } from "react-beautiful-dnd"
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from "next/document";
+import React from "react";
+import { resetServerContext } from "react-beautiful-dnd";
 
-type Props = {}
+import { ColorModeScript } from "@chakra-ui/react";
+import theme from "../styles/theme";
+
+type Props = {};
 
 class MyDocument extends Document<Props> {
-  /* 
+  /*
    * fix the clinet/server error using resetServerContext()
    */
-  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
-    const initialProps = await Document.getInitialProps(ctx)
-    resetServerContext()
-    return { ...initialProps }
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    resetServerContext();
+    return { ...initialProps };
   }
 
   render() {
     return (
-      <Html lang='en'>
+      <Html lang="en">
         <Head></Head>
         <body>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
