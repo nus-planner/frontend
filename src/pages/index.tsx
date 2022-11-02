7f0fa658aa97a2c228ae3b21854cdff4be5a4f46
Ryan Peh <qwertz0808@gmail.com>
2022-11-02 19:11:45 +0800

    Add alert for users on mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,30 @@
-import { Stack, Divider } from "@chakra-ui/react";
+import { Stack, Divider, useDisclosure } from "@chakra-ui/react";
 import { useState, useCallback, useEffect, SetStateAction } from "react";
 import PlannerComponent from "../components/Planner";
-import { labelModules, loadViewModel } from "../utils/plannerUtils";
+import { loadViewModel } from "../utils/plannerUtils";
 import BasicInfo from "../components/BasicInfo";
 import { useAppContext } from "../components/AppContext";
-import { VIEWMODEL_STORAGE } from "../constants/planner";
+import { isMobile } from "react-device-detect";
+import MobileDeviceAlert from "../components/MobileDeviceAlert";
 
 const Home = () => {
   // Helper function to help refresh since react-beautiful-dnd can't detect some changes
   const [, updateState] = useState<{}>();
   const forceUpdate = useCallback(() => updateState({}), []);
   const { mainViewModel, setMainViewModel } = useAppContext();
-  
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   useEffect(() => {
     loadViewModel(mainViewModel);
     forceUpdate();
+    if (isMobile) {
+      onOpen();
+    }
   }, []);
 
   return (
     <Stack padding="1rem">
+      <MobileDeviceAlert isOpen={isOpen} onClose={onClose} />
       <BasicInfo />
       <Divider />
       <PlannerComponent />
