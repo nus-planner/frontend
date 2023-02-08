96e3b77599a0501c3a2145abef586443c8b53e98
Yongbeom-Kim <yongbeom.kim@u.nus.edu>
2023-02-08 11:55:38 +0800

    Add import reflect-metadata to fix class-transformer import

--- src/models/basket.ts
+++ src/models/basket.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata'; // needed for class-transformer https://github.com/typestack/class-transformer/issues/178
 import { Exclude } from "class-transformer";
 import { Module, AcademicPlanView, PropertySetFilter } from "./plan";
 
